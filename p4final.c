3ad10c4f44a363712900b15c87cb7156cb3d050f
sanjanaraibs27 <97089389+SanjanaRai27@users.noreply.github.com>
2022-03-30 16:58:27 +0000

    P4 DONE

--- p4final.c
+++ p4final.c
@@ -1,2 +1,33 @@
-
+#include<stdio.h>
+#include<math.h>
+int input()
+{
+  int n;
+  printf("Enter the value of n for fibonacci\n");
+  scanf("%d",&n);
+  return n;
+}
+int find_fibo(int n)
+{
+  int i,a=0,b=1,sum;
+  for(i=1;i<n;i++)
+    {
+      int temp=b;
+      b=a+b;
+      a=temp;
+    }
+  return a;
+}
+void output(int n,int fibo)
+{
+  printf("The %dth number of the fibonacci sequence is %d\n",n,fibo);
+}
+int main()
+{
+  int a,f;
+  a=input();
+  f=find_fibo(a);
+  output(a,f);
+  return 0;
+}
 
