151a1f75059ccfd911b95c8c4042e268c9b66e42
sanjanaraibs27 <97089389+SanjanaRai27@users.noreply.github.com>
2022-03-30 17:16:13 +0000

    p7 done

--- p7final.c
+++ p7final.c
@@ -1,8 +1,22 @@
 #include<stdio.h>
-struct _point
+struct _point 
 {
-float x,y\n;
+float x,y;
 };
-ty\npe
-def struct _point Point;
-
+typedef struct _point Point;
+struct _line
+{
+Point p1,p2;
+float distance;
+};
+typedef struct _line Line
+Point input_point(p1)
+{
+  printf("Enter the values of x and y\n");
+  scanf("%f%f",$p1.x,&p1.y);
+  return p1;
+}
+Line input_line(l)
+{
+  
+}
